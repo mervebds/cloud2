51552ab204e647143fcde5a15c25522a953fe110
Merve Budus <mervebuduss@gmail.com>
2022-02-24 22:56:04 +0300

    ilk commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
-  Dosya Yükleyin:
+  Dosyayı Burdan Yükleyin:
   <input type="file" name="fileToUpload" id="fileToUpload">
   <input type="submit" value="Upload" name="submit">
 </form>
